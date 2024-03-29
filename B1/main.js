30e67a570e59a51d978633f0c66ce779bf74ecf9
khanhnhat188 <teamka71@gmail.com>
2024-03-29 13:54:49 +0700

    Update map height and heading in index.html and remove unnecessary code in main.js

--- B1/main.js
+++ B1/main.js
@@ -9,26 +9,11 @@ var popup = L.popup()
     .setContent("Xin chào Cần Thơ")
     .openOn(mymap);
 
-
-
-
 var icon = L.icon({
     iconUrl: 'https://lh5.googleusercontent.com/p/AF1QipPaoto4XQu388tFVAcE2ywUcjoUGxELwyj4DU1F=w215-h280-p-k-no',
     iconSize: [30, 30]
 });
 var marker = L.marker([16.4533864, 107.5359133], { icon: icon }).addTo(mymap);
-
-var marker = L.marker([10.0341844, 105.7163704]).addTo(mymap);
-mymap.on('click', function (e) {
-    marker.setLatLng(e.latlng);
-});
-
-//add marker to add to mouse
-var addMarker = (e) => {
-    let marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);
-    marker.bindPopup("Vị trí của bạn").openPopup();
-}
-mymap.on('click', addMarker);
 
 
 
@@ -40,17 +25,6 @@ var googlemap = L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
 var oms = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
-
-var Move = L.popup();
-    function onMapClick(e) {
-        Move
-            .setLatLng(e.latlng)
-            .setContent("Vị trí" + e.latlng)
-            .openOn(mymap);
-    }
-mymap.on('click', onMapClick);
-
-
 
 
 var mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
